216810c9dfaeb1f904f7970dfabf4c5acd150ab6
AmanJaiswal2001 <amanjaisawal537@gmail.com>
2025-02-27 09:41:00 +0530

    new project

---
+++ practice-react/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Movie from './components/Movie'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ <Movie/>
+  )
+}
+
+export default App
---
+++ practice-react/src/components/Movie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Movie = () => {
+  return (
+    <div>
+        <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default Movie
